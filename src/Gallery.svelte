56c9bc796ba895a5872a76ae62400ad82601f6a5
madeleineostoja <madi@madeleineostoja.com>
2021-05-30 15:24:55 +1200

    Add prerender support

--- src/Gallery.svelte
+++ src/Gallery.svelte
@@ -8,7 +8,7 @@
   export let imageComponent = Img;
 
   let scaledImages = [];
-  let width;
+  let width = 0;
 
   function imgStyle({ scaledWidth, scaledHeight, isLastInRow, isLastRow }) {
     let marginRight = gutter + 'px',
@@ -20,22 +20,15 @@
       flex = `1 1 ${scaledWidth - 4}px`;
     }
 
-    return `
-      height: ${scaledHeight}px;
-      flex: ${flex};
-      margin-right: ${marginRight};
-      margin-bottom: ${marginBottom};
-    `;
+    return `height: ${scaledHeight}px; flex: ${flex}; margin-right: ${marginRight}; margin-bottom: ${marginBottom};`;
   }
 
-  $: if (width) {
-    scaledImages = layout({
-      images,
-      containerWidth: width,
-      targetHeight: rowHeight,
-      gutter
-    });
-  }
+  $: scaledImages = layout({
+    images,
+    containerWidth: width || 1280,
+    targetHeight: rowHeight,
+    gutter
+  });
 
 </script>
 
@@ -59,10 +52,14 @@
     height: 100%;
   }
 
+  .hidden {
+    visibility: hidden;
+  }
+
 </style>
 
 <div class="masonry" bind:clientWidth={width}>
-  <div class="container" style="width: {width}px">
+  <div class="container" style="width: {width}px" class:hidden={!width}>
     {#each scaledImages as { index, ratio, scaledHeight, scaledWidth, isLastInRow, isLastRow, scaledWidthPc, ...image }}
       <div
         class="image"
